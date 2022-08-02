444c1fc1cb1a18035cc1969c001ce4f5ba322757
Tony Ryu <tonyappz@gmail.com>
2022-08-02 15:45:02 +0900

    Initial commit

---
+++ src/test/java/com/tonyappz/grpcExam/GrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyappz.grpcExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
